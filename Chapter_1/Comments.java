6cdfce0718f0bb1828fdd3b3f44832d4eb3af32d
ErvicJayOmiping-UCM <ervicjayo1@gmail.com>
2025-09-16 09:05:21 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,12 @@
+public class Comments {
+    public static void main(String[] args) {
+    
+System.out.println(" // - The Single-line comment");// This is a single-line comment
+System.out.println(" /* */ - The Multi-line comment");/* This is a multi-line comment. */
+
+System.out.println(" /** */ - The documentation comment "); /** This is a documentation comment. */
+
+System.out.println("3 differrent types of comment in Java");
+
+}
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,10 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+    
+System.out.println("In The End by Linkin Park,");
+System.out.println("Loser by Sueco,");
+System.out.println("blacknwhite by pdktp,");
+System.out.println("turning chapters by dylan lotus.");
+
+}
+}
